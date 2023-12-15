e168a9b8dbc720d74c5ac9eb768dd6cacf9e7c09
himelhere <ssade.shahriar@gmail.com>
2023-12-15 20:09:57 +0600

    Created serivce booking page functional

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -5,20 +5,62 @@ import { AuthContext } from "../Components/Provider/AuthProvider";
 
 const Booking = () => {
     const {user} = useContext(AuthContext);
-    const { title } = useLoaderData();
-
+    const { _id, title, price, img } = useLoaderData();
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = form.email.value;
+        const price = form.due.value;
+        const order = {
+            customerName: name,
+            email,
+            img,
+            date,
+            service: _id,
+            price
+        }
+        console.log(order);
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(order) 
+        })
+        .then(result => result.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('Successfully added!')
+            }
+        })
+    }
     return (
         <div>
             <NavBar></NavBar>
             <div className="hero min-h-screen bg-base-200">
-            
+             
                 <div className="hero-content flex-col">
                     <div>
                     <h1 className="text-center text-3xl font-bold">Booking the service: {title}</h1>
                     </div>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         
-                        <form className="card-body">
+                        <form onSubmit={handleSubmit} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input defaultValue={user?.displayName} type="text" name="name" placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Date</span>
+                                </label>
+                                <input type="date" name="date" className="input input-bordered" required />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -27,15 +69,13 @@ const Booking = () => {
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text">Due price</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="text" name="due" defaultValue={'$' + price} placeholder="price" className="input input-bordered" required />
+                                
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Book Now</button>
+                                <button type="submit" className="btn btn-primary">Book Now</button>
                             </div>
                         </form>
                     </div>
